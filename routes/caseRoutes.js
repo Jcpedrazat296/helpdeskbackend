336f94c57afd684fbb000312d9da461296cc6c34
jcpedrazat296 <jcpedrazat@sanmateo.edu.co>
2025-12-16 20:41:56 -0500

    Actualizar estado y reasignar casos

--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -1,13 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { createCase, getCases } = require("../controllers/caseController");
-const verifyToken = require("../middlewares/verifyToken");
-
-router.get("/", verifyToken, getCases);
-router.post("/", verifyToken, createCase);
-
-module.exports = router;
-
 const {
   getCases,
   createCase,
