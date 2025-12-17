20e966172cfe1b18411481c1c5ad228b5f7e51c7
jcpedrazat296 <jcpedrazat@sanmateo.edu.co>
2025-12-16 20:44:05 -0500

    Actualizar estado y reasignar casos

--- controllers/caseController.js
+++ controllers/caseController.js
@@ -1,46 +1,6 @@
-const { v4: uuid } = require("uuid");
 const cases = require("../data/cases.json");
 const fs = require("fs");
 const path = require("path");
-
-exports.createCase = (req, res) => {
-  const { titulo, descripcion, categoria } = req.body;
-
-  if (!titulo || !descripcion || !categoria) {
-    return res.status(400).json({ message: "Todos los campos son obligatorios" });
-  }
-
-  const newCase = {
-    id: uuid(),
-    titulo,
-    descripcion,
-    categoria,
-    estado: "abierto",
-    creadoPor: "usuario_externo",
-    asignadoA: "soporte_nivel_1",
-    fechaCreacion: new Date().toISOString(),
-    fechaActualizacion: new Date().toISOString(),
-    historial: [
-      {
-        fecha: new Date().toISOString(),
-        accion: "Caso creado por usuario externo"
-      }
-    ]
-  };
-
-  // Guardar en memoria
-  cases.push(newCase);
-
-  // Guardar en archivo JSON
-  const filePath = path.join(__dirname, "../data/cases.json");
-  fs.writeFileSync(filePath, JSON.stringify(cases, null, 2));
-
-  // Responder al frontend
-  res.status(201).json({
-    message: "Caso creado correctamente",
-    caso: newCase
-  });
-};
 
 exports.updateCase = (req, res) => {
   const { id } = req.params;
